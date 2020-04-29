a9581fb2961695bbf4342d99c414858b1564471a
athoreso <62078132+athoreso@users.noreply.github.com>
2020-04-29 10:51:00 -0500

    Update laser.js

--- game/script/laser.js
+++ game/script/laser.js
@@ -1,1 +1,42 @@
-//this is currently within script.js and will be moved once fully implemented
+
+function Laser(sPos, angle){ //staring position, velocity, and angle
+
+  this.pos = createVector(sPos.x, sPos.y);
+  this.vel = p5.Vector.fromAngle(angle);
+  this.vel.mult(7); //speed up lasers
+  
+  this.update = function() {
+    this.pos.add(this.vel);
+  }
+  
+  this.render = function(){
+    push();
+    stroke(255);
+    strokeWeight(4);
+    point(this.pos.x, this.pos.y);
+    pop();
+  }
+  
+  this.offscreen = function(){
+    if(this.pos.x > width || this.pos.x < 0 ){
+      return true;
+    }else if(this.pos.y > height || this.pos.y < 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
+  this.hits = function(asteroid){
+    var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
+    if(d < asteroid.r){
+      //asteroid.break;
+      //console.log("hit");
+      return true;
+    } else{
+      return false;
+    }
+  }
+  
+  
+}
